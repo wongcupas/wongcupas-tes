e4da0cd29167b17f8dba5eaf2415a8b2b12f8e44
wongcupas <wongcupas@gmail.com>
2023-12-31 17:58:58 +0000

    menambahkan test koneksi pada index.php

--- index.php
+++ index.php
@@ -9,6 +9,11 @@
     <li>Membuat Branch1</li>
     <li>Membuat Branch2</li>
   </ul>
+  <hr/>
+  <h1> Belajar Github connect visual studio code</h1>
+<ul>
+  <li>Tes koneksi</li>
+</ul>
   
   <p>sdfbsdkfbksadbf saj bfsabdl fjasflsa fvl salfsald fvlsa lsa lfsal loren vbadjsvl sa</p>
   <p>sdfbsdkfbksadbf saj bfsabdl fjasflsa fvl salfsald fvlsa lsa lfsal loren vbadjsvl sa</p>
